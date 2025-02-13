9eb57dfde409c6daba21b3818bcceef3304b3ce1
Charles <chaolinjin@gmail.com>
2025-02-13 14:25:07 +0800

    Update ui-tars.rb

--- Casks/u/ui-tars.rb
+++ Casks/u/ui-tars.rb
@@ -1,11 +1,12 @@
 cask "ui-tars" do
   arch arm: "arm64", intel: "x64"
+
   version "0.0.5"
   sha256 :no_check
 
   url "https://github.com/bytedance/UI-TARS-desktop/releases/download/v#{version}/UI.TARS-#{version}-#{arch}.dmg"
   name "UI-TARS Desktop"
-  desc "A GUI Agent application based on UI-TARS(Vision-Lanuage Model) that allows you to control your computer using natural language"
+  desc "GUI Agent for computer control using UI-TARS vision-language model"
   homepage "https://github.com/bytedance/UI-TARS-desktop"
 
   livecheck do
@@ -22,6 +23,6 @@ cask "ui-tars" do
 
   zap trash: [
     "~/Library/Application Support/ui-tars-desktop",
-    "~/Library/Logs/ui-tars-desktop"
+    "~/Library/Logs/ui-tars-desktop",
   ]
 end
